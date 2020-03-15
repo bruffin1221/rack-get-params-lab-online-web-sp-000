9dff80bc04f2bf6adc0dd4f8e1d52bf657cc9aae
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-15 06:15:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -30,7 +30,7 @@ class Application
           if @@items.include?(add_item)
              @@cart<<add_term
              resp.write "added #{add_item}"
-           elsif
+           else
             resp.write "We don't have that item"
         end
         else resp.write "Path Not Found"
