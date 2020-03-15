eede7953cdd128e539b2be18dca237d9dbf51cb4
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-15 05:33:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
 
           add_term = req.params["q"]
 
-          if @@items.none?             r
+          if @@items.none?             
             esp.write "We don't have that item"
           elsif @@items.include?(add_term)
              @@cart<<add_term
