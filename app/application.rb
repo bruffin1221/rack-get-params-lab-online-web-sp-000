cced09746386d6eb597ff25f4eb3ec72567b3bbc
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-15 05:51:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -30,7 +30,7 @@ class Application
 
           if @@items.include?(add_term)
              @@cart<<add_term
-             resp.write "added #{added_term}" 
+             resp.write "added #{add_term}" 
            else
             resp.write "We don't have that item"
         end
